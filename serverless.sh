edb1c3783b31ba119ab699f840d7b0ab61c869e0
14mar1983 <14mar1983@gmail.com>
2017-08-22 08:17:24 +0530

    Update serverless.sh

--- serverless.sh
+++ serverless.sh
@@ -3,5 +3,4 @@ OPTION="-s $STAGE -r $AWS_REGION"
 npm install -g npm
 npm install -g serverless
 npm i serverless-plugin-custom-domain
-serverless config credentials --provider aws --key AKIAJIIH7PAPOAHJ57EQ --secret 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 --aws-profile support-circle-ci
 sls deploy -v1
