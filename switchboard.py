79e6fa21b129f30a0f8524d5347649c641e16448
14mar1983 <14mar1983@gmail.com>
2017-08-23 10:09:34 +0000

    Update switchboard.py

--- switchboard.py
+++ switchboard.py
@@ -3,7 +3,8 @@ import os
 
 
 def _airflow_request(p_event_type, p_event_dict):
-    import requests
+    python
+    >>> import requests
     v_config_dict = _get_config_dict()
     v_airflow_request_url = v_config_dict['airflow']['dag_urls'][p_event_type]
     try:
